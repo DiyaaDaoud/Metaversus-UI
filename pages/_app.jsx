502e54e55e89fd7a85b2a3719484b6c7c085cfd0
DiyaaDaoud <didoodidoo2312@gmail.com>
2023-07-17 03:17:20 +0300

    adding meta tags

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,6 +7,22 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <title>Metaversus</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Metaversus UI website" />
+      {/** google */}
+      <meta itemProp="name" content="Metaversus" />
+      <meta itemProp="description" content="Metaversus UI website" />
+      <meta
+        itemProp="image"
+        content="https://metaversus-ui-three.vercel.app/favicon.ico"
+      />
+      {/** google */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Metaversus" />
+      <meta itemProp="twitter:description" content="Metaversus UI website" />
+      <meta
+        itemProp="twitter:image"
+        content="https://metaversus-ui-three.vercel.app/favicon.ico"
+      />
     </Head>
     <Component {...pageProps} />
   </>
